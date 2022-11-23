2dd16c19f454b43b1e06d9884617ba515a60b285
Abdul Hanan Asif <59803552+abdulhanan5643@users.noreply.github.com>
2022-11-23 17:39:56 +0500

    feat: get data from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import axios from "axios";
+import React, { useEffect, useContext } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const fetchFacts = async () => {
+      let data
+      try {
+        await axios.get('https://catfact.ninja/fact').then(res => {
+          return res.data.fact
+        })
+      } catch (err) {
+        data = { success: false, message: 'There was an error with provided endpoint!', error: err }
+      }
+      return data;
+    }
+    fetchFacts()
+  }, [])
   return <div className="App"></div>;
 }
 
