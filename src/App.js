aeb794d59086d69b1ecd6583c927167102c1e3b3
Abdul Hanan Asif <59803552+abdulhanan5643@users.noreply.github.com>
2022-11-23 17:40:59 +0500

    feat: facts stored in context api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import React, { useEffect, useContext } from "react";
 import "./App.css";
+import GlobalContext from "./context/GlobalContext";
 
 function App() {
+  const { updateFacts } = useContext(GlobalContext)
   useEffect(() => {
     const fetchFacts = async () => {
       let data
       try {
         await axios.get('https://catfact.ninja/fact').then(res => {
-          return res.data.fact
+          updateFacts(res.data.fact)
         })
       } catch (err) {
         data = { success: false, message: 'There was an error with provided endpoint!', error: err }
@@ -17,6 +19,7 @@ function App() {
     }
     fetchFacts()
   }, [])
+
   return <div className="App"></div>;
 }
 
